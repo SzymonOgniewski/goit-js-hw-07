2fea7eac68ed953035339874d49a832f10d293a1
Szymon Ogniewski <szymonogniewski00@gmail.com>
2022-11-06 21:15:19 +0100

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const instance = basicLightbox.create(
+  `
+    <div class="modal">
+       <img src="" alt = "original image"/>
+    </div>
+`,
+  {
+    closable: true,
+  }
+);
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryItems.forEach((element) => {
+  const item = document.createElement("div");
+  item.classList.add("gallery__item");
+  gallery.append(item);
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = element.original;
+  item.append(link);
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = element.preview;
+  image.alt = element.description;
+  image.dataset.source = element.original;
+  link.append(image);
+});
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  const clickedElement = event.target;
+  if (clickedElement.nodeName !== "IMG") return;
+  const originalImg = clickedElement.dataset.source;
+  const modal = instance.element();
+  const modalImg = modal.querySelector("img");
+  const description = clickedElement.alt;
+  modalImg.src = originalImg;
+  modalImg.alt = description;
+  instance.show();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key !== "Escape") return;
+  if (instance.visible()) instance.close();
+});
